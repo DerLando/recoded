24881540c88d8e389108790d9adb03d9a128958c
Lando <lando.schumpich@gmail.com>
2024-05-23 21:13:54 +0200

    Add naive save function
    This still needs optimization to make
    sure that no actual data is saved to
    the file, but rather only nodes and
    their connectivity. The data can be
    recreated by solving the graph once.
    This is especially important as we want
    the file to be stored server-side.

--- src/app.rs
+++ src/app.rs
@@ -42,6 +42,28 @@ impl App for NodeGraphApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui_extras::install_image_loaders(ctx);
 
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Save").clicked() {
+                        // TODO: Saving the file shows that all
+                        // data of the node inputs is stored in
+                        // the file itself, which is bad, we ONLY
+                        // want to store the nodes and their
+                        // connectivity, as well as options on nodes
+                        // and re-solve the graph on load to create
+                        // the data at runtime instead!
+                        let serialized = serde_json::to_string_pretty(&self.snarl).unwrap();
+                        std::fs::write("graph.json", serialized);
+                    }
+                    if ui.button("Load").clicked() {}
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close)
+                    }
+                })
+            })
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             self.snarl.show(
                 &mut NodeGraphViewer,
